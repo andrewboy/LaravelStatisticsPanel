85f99fb9ddd6086ad226b28bea379ccf37bb1ff0
unknown <beck.andras@989096CE5198.phgroup.local>
2016-06-13 17:04:01 +0200

    fix

--- src/resources/assets/js/compiled/jquery.laravel_statistics_panel.js
+++ src/resources/assets/js/compiled/jquery.laravel_statistics_panel.js
@@ -505,6 +505,17 @@ window.statistics_panel.boxes = (function () {
         this.source = source;
     
         StatisticsBox.apply(this, Array.prototype.slice.call(arguments));
+    
+        var self = this;
+    
+        $('input.stat_interval', this.context).datepicker({
+            format: "yyyy/mm",
+            startView: "months",
+            minViewMode: "months",
+            autoclose: true
+        }).on('changeDate', function () {
+            self.fetchDatas();
+        });
     }
     
     MonthlyBox.prototype = new StatisticsBox();
@@ -518,18 +529,44 @@ window.statistics_panel.boxes = (function () {
         //    self.updateWidgets(data);
         //});
     
-        var self = this;
+        //==============
     
-        $('input.stat_interval', this.context).datepicker({
-            format: "yyyy/mm",
-            startView: "months",
-            minViewMode: "months",
-            autoclose: true
-        }).on('changeDate', function () {
-            self.onDateChange();
+        //var self = this;
+        //
+        //$('input.stat_interval', this.context).datepicker({
+        //    format: "yyyy/mm",
+        //    startView: "months",
+        //    minViewMode: "months",
+        //    autoclose: true
+        //}).on('changeDate', function () {
+        //    self.onDateChange();
+        //});
+        //
+        //$('input.stat_interval', this.context).first().trigger('changeDate');
+    
+        var self = this, i, params,
+            dateFromParams = $('input.stat_interval.from', this.context).val().split("/");
+    
+        for (i in this.widgetContainer) {
+            if (this.widgetContainer.hasOwnProperty(i)) {
+                this.widgetContainer[i].setLoadIcon(true);
+            }
+        }
+    
+        params = {
+            'in': {
+                year: dateFromParams[0],
+                month: dateFromParams[1]
+            }
+        };
+    
+        $.ajax({
+            type: 'GET',
+            url: this.source,
+            data: params
+        }).done(function (data) {
+            self.updateWidgets(data);
         });
-    
-        $('input.stat_interval', this.context).first().trigger('changeDate');
     };
 
     return {
