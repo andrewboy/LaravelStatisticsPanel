1efb27dc9ff87f9226897ce41cdab3e85b3cc67e
unknown <beck.andras@989096CE5198.phgroup.local>
2016-06-17 16:51:07 +0200

    fix

--- src/resources/assets/js/boxes/TimeIndependentBox.js
+++ src/resources/assets/js/boxes/TimeIndependentBox.js
@@ -1,50 +1,21 @@
 function TimeIndependentBox(context, source) {
     this.context = context;
     this.source = source;
+
+    StatisticsBox.apply(this, Array.prototype.slice.call(arguments));
 }
 
-TimeIndependentBox.prototype.init = function () {
-    var obj = this;
-    $('.stat-widget', this.context).each(function (i, item) {
-        obj.setWidget($(item));
-    });
-
-    this.boot();
-};
+TimeIndependentBox.prototype = new StatisticsBox();
 
 TimeIndependentBox.prototype.boot = function () {
-    $('.stat-widget', this.context).each(function (i, item) {
-        $(item).data('widget').setLoadIcon(true);
-    });
-
     this.fetchDatas();
-};
-
-TimeIndependentBox.prototype.updateWidgets = function (data) {
-    var id,
-        statWidgets = $('.stat-widget', this.context);
-
-    for (id in data) {
-        var $stat = statWidgets.filter('[data-stat-id="' + id + '"]');
-
-        if ($stat.length > 0) {
-            $stat.data('widget').update(data[id]);
-            $stat.data('widget').setLoadIcon(false);
-        }
-    }
-};
-
-TimeIndependentBox.prototype.setWidget = function ($item) {
-    var id = $item.data('statId');
-
-    var widget = new window.statistics_panel.stat_widgets[$item.data('type')]($item);
-    widget.init($item);
-
-    $item.data('widget', widget);
 };
 
 TimeIndependentBox.prototype.fetchDatas = function () {
     var self = this;
+
+    this.setLoadIcons(true);
+
     $.ajax({
         type: 'GET',
         url: self.source
